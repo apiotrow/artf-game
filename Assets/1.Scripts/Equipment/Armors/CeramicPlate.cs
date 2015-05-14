0bb43e98fc0d70d995e5bff93b907e50f6c73884
Andrew Miller <thedr111111@yahoo.com>
2015-05-13 21:12:33 -0700

    fixed naming error

--- Assets/1.Scripts/Equipment/Armors/CeramicPlate.cs
+++ Assets/1.Scripts/Equipment/Armors/CeramicPlate.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Poncho : Armor {
+public class CeramicPlate : Armor {
 	// Used for setting stats for each weapon piece
 	protected override void setInitValues() {
 		base.setInitValues();
